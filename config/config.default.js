479daef06c2525ff05e94d812fd7274040db5863
Clanaid <miku6590@hotmail.com>
2021-06-29 14:30:21 +0800

    fix: reader extended option attributes
    * the `sub` option in the default configuration file should be empty

--- config/config.default.js
+++ config/config.default.js
@@ -10,17 +10,17 @@ exports.nsqjs = {
     lookupdHTTPAddresses: 'YOUR_HOST:PORT', // string or array of strings
     sub: {
       // key is subscription code file name
-      key: {
-        //   topic: '',
-        //   channel: '',
-        //   // more options refer to the document of nsqjs Reader.
-        //   opts: {},
-      },
+      // key: {
+      //   topic: '',
+      //   channel: '',
+      //   // more options refer to the document of nsqjs Reader.
+      //   opts: {},
+      // },
     },
   },
   writer: {
-    nsqdHost: 'YOUR_HOST',
-    nsqdPort: 4150,
+    // nsqdHost: 'YOUR_HOST',
+    // nsqdPort: 4150,
     // more options refer to the document of nsqjs Writer.
     // options:{}
   },
